c0cbe032edf5a5e3b543b95d240571ee00a560aa
ANGEL LUIS SAORIN FAURA <alsaorin9987@gmail.com>
2023-11-08 09:26:22 +0100

    Apertura Formularios hijo

---
+++ ejercicioClases_021123/Tienda/Tienda/FormConsultaArticulo.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Tienda
+{
+    partial class FormConsultaArticulo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(437, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consulta de Articulos";
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnInicio.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnInicio.FlatAppearance.BorderSize = 0;
+            this.btnInicio.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LawnGreen;
+            this.btnInicio.FlatAppearance.MouseOverBackColor = System.Drawing.Color.GreenYellow;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.ForeColor = System.Drawing.Color.LightCyan;
+            this.btnInicio.Location = new System.Drawing.Point(548, 330);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(145, 41);
+            this.btnInicio.TabIndex = 2;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseCompatibleTextRendering = true;
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // FormConsultaArticulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1198, 615);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximumSize = new System.Drawing.Size(1198, 615);
+            this.MinimumSize = new System.Drawing.Size(861, 456);
+            this.Name = "FormConsultaArticulo";
+            this.Text = "FormConsultaArticulo";
+            this.Load += new System.EventHandler(this.FormConsultaArticulo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInicio;
+    }
+}
--- ejercicioClases_021123/Tienda/Tienda/FormPrincipal.cs
+++ ejercicioClases_021123/Tienda/Tienda/FormPrincipal.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Tienda
@@ -22,8 +17,53 @@ namespace Tienda
         {
 
         }
-        #region Funcionalidades del formulario
-        //RESIZE METODO PARA REDIMENCIONAR/CAMBIAR TAMAÑO A FORMULARIO EN TIEMPO DE EJECUCION ----------------------------------------------------------
+
+        // LA SIGUIENTE DECLARACION " #region... " ES DECLARADA CON EL FIN DE SEPARAR TODO EL CÓDIGO
+        // SEGÚN SU FUNCIONALIDAD DENTRO DE UN MISMO PROGRAMA. CONTRIBUYE AL ENCAPSULADO DE CADA
+        // BLOQUE DE FUNCIONALIDADES PUDIENDO CONTRAERLO Y, ASÍ TENER MÁS DESPEJADA LA INTERFAZ
+        // DE LA HERRAMIENTA IDE CON LA QUE ESTAMOS TRABAJANDO.
+
+        #region Funcionalidades y Estilos del formulario 
+
+        Panel p = new Panel();       
+        private void btnMouseEnter(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            panelNavBar.Controls.Add(p);
+            p.BackColor = Color.GreenYellow;
+            p.Size = new Size(107, 5);
+            p.Location = new Point(btn.Location.X, btn.Location.Y + 40);
+        }
+
+        private void btnMouseLeave(object sender, EventArgs e)
+        {
+            panelNavBar.Controls.Remove(p);
+        }
+
+        Panel pSubArt = new Panel();
+        Panel pSubPed = new Panel();
+                    
+        private void btnMouseEnterSub(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            panelSubmenuArticulos.Controls.Add(pSubArt);
+            pSubArt.BackColor = Color.GreenYellow;
+            pSubArt.Size = new Size(107, 5);
+            pSubArt.Location = new Point(btn.Location.X, btn.Location.Y + 40);
+
+            panelSubmenuPedidos.Controls.Add(pSubPed);
+            pSubPed.BackColor = Color.GreenYellow;
+            pSubPed.Size = new Size(107, 5);
+            pSubPed.Location = new Point(btn.Location.X, btn.Location.Y + 40);
+        }
+
+        private void btnMouseLeaveSub(object sender, EventArgs e)
+        {
+            panelNavBar.Controls.Remove(pSubArt);
+            panelNavBar.Controls.Remove(pSubPed);
+        }
+        // "RESIZE": METODO PARA REDIMENSIONAR/CAMBIAR TAMAÑO A FORMULARIO EN TIEMPO DE EJECUCION 
+
         private int tolerance = 12;
         private const int WM_NCHITTEST = 132;
         private const int HTBOTTOMRIGHT = 17;
@@ -45,7 +85,7 @@ namespace Tienda
             }
         }
 
-        //----------------DIBUJAR RECTANGULO / EXCLUIR ESQUINA PANEL
+        // DIBUJAR RECTANGULO / EXCLUIR ESQUINA PANEL
         protected override void OnSizeChanged(EventArgs e)
         {
             base.OnSizeChanged(e);
@@ -58,38 +98,24 @@ namespace Tienda
             this.Invalidate();
         }
 
-        //----------------COLOR Y GRIP DE RECTANGULO INFERIOR
+        // COLOR Y GRIP DE RECTANGULO INFERIOR
         protected override void OnPaint(PaintEventArgs e)
         {
             SolidBrush blueBrush = new SolidBrush(Color.FromArgb(244, 244, 244));
             e.Graphics.FillRectangle(blueBrush, sizeGripRectangle);
 
             base.OnPaint(e);
-            ControlPaint.DrawSizeGrip(e.Graphics, Color.Transparent, sizeGripRectangle);
+            ControlPaint.DrawSizeGrip(e.Graphics, Color.LightGray, sizeGripRectangle);
         }
 
-        //METODO PARA ARRASTRAR EL FORMULARIO---------------------------------------------------------------------
-
-        // ENFOQUE FUNCION SOBRE PANEL SUPERIOR DEFINIDO
-        private void panelTop_MouseMove(object sender, MouseEventArgs e)
-        {
-            ReleaseCapture();
-            SendMessage(this.Handle, 0x112, 0xf012, 0);
-        }
-
-        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
-        private extern static void ReleaseCapture();
-
-        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
-        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
-        #endregion
-
+        // VINCULACION EVENTO CIERRE DE APP A PARTIR DE UN PICTURE_BOX MODIFICADO
         private void btnCerrar_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-        // CAPTURAR POSICION VECTORIAL Y TAMAÑO ANTES DE MAXIMIZAR PARA RESTAURAR
+        // CAPTURAR POSICION VECTORIAL BOTONES DE EVENTOS DE VENTANA MAXIMIZAR, MINIMIZAR Y RESTAURAR
+
         int lx, ly;
         int sw, sh;
 
@@ -113,11 +139,81 @@ namespace Tienda
             this.Location = Screen.PrimaryScreen.WorkingArea.Location;
         }
 
-
-        // FUNCION CIERRE APP A PARTIR DE PICTURE_BOX MODIFICADO
         private void btnMinimizar_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        // METODO PARA ARRASTRAR EL FORMULARIO. EVENTO VINCULADO EN PANEL SUPERIOR (NAME : panelTop)
+
+        private void panelTop_MouseMove(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        private void btnAltaArticulo_Click(object sender, EventArgs e)
+        {
+            AbrirFormHijo<FormAltaArticulo>();
+        }
+
+        private void btnConsultaArticulo_Click(object sender, EventArgs e)
+        {
+            AbrirFormHijo<FormConsultaArticulo>();
+        }
+
+        private void btnDetallePedido_Click(object sender, EventArgs e)
+        {
+            AbrirFormHijo<FormDetallePedido>();
+        }
+
+        private void btnArticulos_Click(object sender, EventArgs e)
+        {
+            panelSubmenuArticulos.Visible = false;
+            panelSubmenuArticulos.Visible = true;
+        }
+
+        private void btnPedidos_Click(object sender, EventArgs e)
+        {
+            panelSubmenuPedidos.Visible = false;
+            panelSubmenuPedidos.Visible = true;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        #endregion
+
+
+        // METODO APERTURA OTROS FORMULARIOS DENTRO DEL PANEL ASIGNADO EN FORMULARIO PRINCIPAL
+        // DECLARACION FORMULARIO GENERICO (Padre):
+        // - CONSTRUCTOR VACIO DESDE DONDE SON LLAMADOS EL RESTO (Hijos).
+
+        private void AbrirFormHijo<FormPadre>() where FormPadre : Form, new()
+        {
+            Form fH;
+
+            // BUSCARA EL FORMULARIO EN LA COLECCION DE <FormPadre>
+            fH = panelBody.Controls.OfType<FormPadre>().FirstOrDefault();
+
+            // CONTROL DE ERRORES O DE FORMULARIO NO EXISTENTE
+            if (fH == null)
+            {
+                fH = new FormPadre();
+                fH.TopLevel = false;
+                fH.Dock = DockStyle.Fill;
+                panelBody.Controls.Add(fH);
+                panelBody.Tag = fH;
+                fH.Show();
+                fH.BringToFront();
+            }
+            else
+            {
+                fH.BringToFront();
+            }
+
         }
     }
 }
